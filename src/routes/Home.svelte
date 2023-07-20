8dc93dfa3de561df0216e7f7b4f4688cd83c07ca
Grace <gfagan98@gmail.com>
2023-07-20 13:11:07 -0400

    feat: favicon

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -6,10 +6,13 @@
   };
 </script>
 
-<main class="p-4 flex flex-col gap-2 h-screen items-center justify-center">
-  <h1 class="font-bold text-3xl">Run There</h1>
+<main class="p-4 flex flex-col gap-6 h-screen items-center justify-center">
+  <div class="flex flex-col gap-1 items-center">
+    <h4>{'ミᕕ( •_•)ᕗ'}</h4>
+    <h1 class="font-bold text-3xl">Run There</h1>
+  </div>
   <button
-    class="p-2 border border-gray-300 rounded-md hover:scale-105 transition-all"
+    class="p-2 rounded-md hover:scale-105 transition-all bg-[#fc4c02] text-white font-semibold"
     on:click={handleLogin}>Connect to Strava</button
   >
 </main>
