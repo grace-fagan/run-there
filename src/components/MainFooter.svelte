b1b897a4babe405b71d04f66ed262bf42a51f12d
Grace <gfagan98@gmail.com>
2023-07-26 16:26:34 -0400

    feat: launch copy

---
+++ src/components/MainFooter.svelte
@@ -0,0 +1,12 @@
+<div class="fixed bottom-0 flex w-full justify-center items-center py-4 gap-6">
+  <a href="https://github.com/grace-fagan/run-there">
+    <button class="fa-brands fa-github text-xl text-[#999999]" />
+  </a>
+  <p class="secondary text-[#999999]">
+    <strong>©</strong>2023
+    <span class="hover:underline cursor-pointer"
+      ><a href="https://github.com/grace-fagan">Grace Fagan</a></span
+    >
+  </p>
+  <img class="h-5 md:h-6" src="/static/img/api-horiz-gray.png" alt="Powered by Strava" />
+</div>
